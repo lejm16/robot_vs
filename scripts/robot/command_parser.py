c794443490da318cfc242f3c4465f862680b32fd
Xqrion <2436801962@qq.com>
2026-03-06 06:56:05 -0800

    feat: 添加机器人参数配置和真实世界启动文件

--- scripts/robot/command_parser.py
+++ scripts/robot/command_parser.py
@@ -18,12 +18,12 @@ class CommandParser:
         """解析指令:导航目标已由move_base处理,这里只处理攻击/行为模式"""
         # 1. 处理攻击指令
         if msg.attack:
-            rospy.loginfo("[{}] 收到攻击指令！".format(self.ns))
+            rospy.loginfo("I am car, [{}] 收到攻击指令！".format(self.ns))
             # 调用attack_executor执行攻击（预留接口）
             # self.execute_attack()
         # 2. 处理行为模式
         mode_map = {0: "待机",1: "巡逻", 2: "攻击", 3: "撤退"}
-        rospy.loginfo("[{}] 当前模式：{}".format(self.ns,mode_map[msg.mode]))
+        rospy.loginfo("I am car, [{}] 当前模式：{}".format(self.ns,mode_map[msg.mode]))
 
     def execute_attack(self):
         """攻击逻辑（预留，可扩展：控制机械臂/发射装置等）"""
--- scripts/robot/red_robot.py
+++ scripts/robot/red_robot.py
@@ -1,5 +1,7 @@
-from command_parser import CommandParser
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import rospy
+from command_parser import CommandParser
 
 class RedRobot:
     def __init__(self):
@@ -9,7 +11,7 @@ class RedRobot:
         self.parser = CommandParser(self.ns)
 
         self.rate = rospy.Rate(10)  # 频率
-        rospy.loginfo("红方TeamManager启动完成")
+        rospy.loginfo("红方robot启动完成")
 
     def run(self):
         while not rospy.is_shutdown():
